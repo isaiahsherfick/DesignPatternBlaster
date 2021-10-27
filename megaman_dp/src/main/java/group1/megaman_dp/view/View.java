e02034010d3370bd53227109ed0eb7d19800d201
Isaiah Sherfick <isherfic@iu.edu>
2021-10-27 12:08:10 -0400

    Created view class just to allow for testing observer/observable stuff in model

---
+++ megaman_dp/src/main/java/group1/megaman_dp/view/View.java
@@ -0,0 +1,12 @@
+package group1.megaman_dp.view;
+
+import group1.megaman_dp.interfaces.Observer;
+
+public class View implements Observer
+{
+	public void update()
+	{
+		//Get list of game objects from the model
+		//Use an iterator to draw everything in that list
+	}
+}
