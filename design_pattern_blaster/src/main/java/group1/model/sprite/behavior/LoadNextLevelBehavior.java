156d875d7926dc9426d85274741abd0acc4982cf
mjawad <mjawad@iu.edu>
2021-12-02 19:36:13 -0500

    FEATURE: Levels now retain score upon loadnextlevel behavior

--- design_pattern_blaster/src/main/java/group1/model/sprite/behavior/LoadNextLevelBehavior.java
+++ design_pattern_blaster/src/main/java/group1/model/sprite/behavior/LoadNextLevelBehavior.java
@@ -7,10 +7,22 @@ import group1.model.sprite.Sprite;
 
 public class LoadNextLevelBehavior implements Behavior
 {
+	int scoreToRegister = 0;
+	public LoadNextLevelBehavior(){
 
+	}
+
+	/**
+	 * Used when the score for the just-completed level needs to be registered
+	 * @param score the score to register
+	 */
+	public LoadNextLevelBehavior(int score){
+		scoreToRegister = score;
+	}
 	@Override
 	public void performBehavior(Sprite sprite) 
 	{
+		App.model.getCurrentLevel().setLevelScore(scoreToRegister);
 		App.model.loadNextLevel();
 	}
 	
